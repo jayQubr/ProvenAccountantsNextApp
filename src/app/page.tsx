90381b8cf62315a8e81dfeb0e7b00b5a7573b351
JayQuber <jay@marketingpanda.com.au>
2025-03-05 03:02:53 +0600

    Implement dashboard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import AppLayout from "./(appLayout)/layout";
+import Dashboard from "./(appLayout)/dashboard/page";
 
 export default function Home() {
   return (
     <AppLayout>
-  <></>
+      <Dashboard />
     </AppLayout>
   );
 }
